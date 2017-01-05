3c09656da3f9e56c418a747097166030fc417d22
KoalaMary <mariiabaeva@yandex.ru>
2017-01-05 17:57:18 +0300

    done

--- plugin.js
+++ plugin.js
@@ -1,24 +1,72 @@
 'use strict'
 
-$(document).ready(function() {
+$(document).ready(function () {
 
-  (function($){
-    $.fn.myPlugin = function(){
+    (function ($) {
+        $.fn.myPlugin = function (options) {
 
-      $('.item').on('click', function() {
-        let height = $(this).outerHeight(true) + parseInt($(this).css('margin-bottom'));
-        let top = $(this).offset().top - $('.item').first().offset().top;
-        let oldMargin = $(this).next('.item').css('margin-top');
-        let newMargin = parseInt($(this).next('.item').css('margin-top')) + height;
-        $(this).css({'position' : 'absolute', 'top' : top}).animate({left: '+=250'}, 1000).animate({top: 0}, 1000);
-        $(this).next('.item').css({'margin-top' : newMargin});
-        $('.item').first().animate({'margin-top' : height},1000);
-        $(this).animate({left: '-=250'}, 1000);
-        $(this).next('.item').animate({'margin-top' : oldMargin},1000);
-      })
+            const settings = $.extend({
+                direction: 'down',
+                index: 8,
+                debug: false
+            }, options);
 
-    };
-  })(jQuery);
+            let initialList = $(this).find('.item');
+            $('.revert').on('click', function revert () {
+               $('.list').empty().append(initialList);
+            });
 
-  $('.list').myPlugin();
+            $(this).on('click', 'li', function () {
+                let lastElement = $('.item').last();
+
+                if (Math.abs(settings.index) <= lastElement.index()) {
+                    var indexElement = $('.item:eq(' + settings.index + ')');
+                } else {
+                    var indexElement = lastElement;
+                }
+
+                if ($(this).index() != indexElement.index()) {
+                    let moveToLeft = $(this).outerWidth() + 25;
+
+                    if (settings.direction === 'up') {
+                        var offset = -($(this).offset().top - indexElement.offset().top);
+                    }
+                    if (settings.direction === 'down') {
+                        var offset = indexElement.offset().top + indexElement.outerHeight() - $(this).offset().top;
+                    }
+
+                    function insertElement(element) {
+                        if (settings.direction === 'up') {
+                            element.insertBefore(indexElement);
+                        }
+                        if (settings.direction === 'down') {
+                            element.insertAfter(indexElement);
+                        }
+                    }
+
+                    function Animation(element) {
+                        element.animate({left: moveToLeft}, 300);
+                        element.animate({top: offset}, 200);
+                        element.animate({left: 0}, 300, function () {
+                            insertElement(element);
+                            element.css({'top': '0'});
+                        });
+                    }
+
+                    Animation($(this));
+
+                    if (settings.debug) console.log($('.item'));
+                }
+            });
+
+            $(this).sortable();
+
+        };
+    })(jQuery);
+
+    $('.list').myPlugin({
+        direction: 'up',
+        index: 2,
+        debug: true
+    });
 })
